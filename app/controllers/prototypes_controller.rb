713967ae5ead5903dbe0efaa596db951a6e6c45d
hiroki0602 <hiroki.fujimori@icloud.com>
2022-07-06 20:19:29 +0900

    実装課題の提出修正版

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+ 
   def index
     @prototypes = Prototype.includes(:user)
     @user = current_user
@@ -28,16 +28,16 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
-    if prototype.save
-      redirect_to root_path
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+    if @prototype.save
+      redirect_to prototype_path
     else
       render :edit
     end
